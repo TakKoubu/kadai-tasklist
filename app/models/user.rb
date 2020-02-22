68d626552973c67d0f9fa1d38e4e02a30c24e808
TakKoubu <kokubu.tk@gmail.com>
2020-02-22 05:55:24 +0000

    submit

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
     uniqueness: { case_sensitive: false }
     has_secure_password
     has_many :tasks
+    has_many :relationships
+    has_many :favoritings, through: :relationships, source: :favorite
+    has_many :reverses_of_relationship, class_name: "Relationship", foreign_key: "favorite_id"
+    has_many :favoriteds, through: :relationships, source: :user
 end
