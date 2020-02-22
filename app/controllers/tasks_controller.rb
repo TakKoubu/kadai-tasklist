b6ef22c99e87f9d5eced17014015c68da05a9706
TakKoubu <kokubu.tk@gmail.com>
2020-02-22 04:27:37 +0000

    fix_pointed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,16 +3,12 @@ class TasksController < ApplicationController
     before_action :correct_user, only: [:show, :edit, :update, :destroy]
     
     def index
-        if logged_in?
+        
           @task = current_user.tasks.build
           @tasks = current_user.tasks.order(id: :desc).page(params[:page])
-        else
-            redirect_to login_path
-        end
     end
     
     def show
-        @task = Task.find_by(id: params[:id])
     end
     
     def new
@@ -33,11 +29,9 @@ class TasksController < ApplicationController
     end
     
     def edit
-        @task = Task.find_by(id: params[:id])
     end
     
     def update
-        @task = current_user.tasks.find_by(id: params[:id])
         if @task.update(task_params)
             flash[:success] = "更新成功"
             redirect_to root_url
